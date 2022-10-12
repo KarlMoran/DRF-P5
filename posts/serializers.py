5ebb6715dfda2280ff2917da6b67b62bf65ef2b0
Karl Moran <karlmoran06@gmail.com>
2022-10-12 11:03:40 +0000

    Adding extra fields to Posts and Profiles

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,86 +1,54 @@
 from rest_framework import serializers
+from posts.models import Post
 from likes.models import Like
-from .models import Post
 
 
-# Class provided by DRF-API walkthrough.
 class PostSerializer(serializers.ModelSerializer):
-    """
-    Serializer for Posts Model.
-    """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     like_id = serializers.SerializerMethodField()
-    likes_count = serializers.ReadOnlyField()
-    comments_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
-        """
-        Restrict image size when uploading.
-        A error warning is provided.
-        """
         if value.size > 2 * 1024 * 1024:
-            raise serializers.ValidationError(
-                'Image must be smaller than 2MB'
-                )
-
+            raise serializers.ValidationError('Image size larger than 2MB!')
         if value.image.height > 4096:
             raise serializers.ValidationError(
-                'Height must be smaller than 4096px'
-                )
-
+                'Image height larger than 4096px!'
+            )
         if value.image.width > 4096:
             raise serializers.ValidationError(
-                'Width must be smaller than 4096px'
+                'Image width larger than 4096px!'
             )
-
         return value
 
     def get_is_owner(self, obj):
-        """
-        Return correct user.
-        """
         request = self.context['request']
         return request.user == obj.owner
 
     def get_like_id(self, obj):
-        """
-        Return & calculate total number
-        of likes on post view.
-        """
         user = self.context['request'].user
-
         if user.is_authenticated:
             like = Like.objects.filter(
-                owner=user,
-                post=obj
+                owner=user, post=obj
             ).first()
             return like.id if like else None
-
         return None
 
     class Meta:
-        """
-        Display fields for views.
-        """
         model = Post
         fields = [
             'id',
             'owner',
-            'is_owner',
-            'profile_id',
-            'profile_image',
             'created_at',
-            'updated_at',
-            'title',
+            'modified_on',
+            'first_name',
+            'last_name',
+            'country',
+            'username',
             'description',
             'image',
-            'like_id',
-            'likes',
-            'likes_count',
-            'comments_count',
+            'is_owner',
             'image_filter',
-            'camera_filter',
         ]
--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,21 +1,26 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 
 class ProfileSerializer(serializers.ModelSerializer):
-    """
-    Profile serializer.
-    Providing readability to profile data in API.
-    """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
-        """
-        Associates User to User Profile.
-        """
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            # print(following)
+            return following.id if following else None
+        return None
 
     class Meta:
         """
